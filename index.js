88a6eebb531f75b10d8173b9243ff94b977901a5
briimiin <kipkorirbrimin005@gmail.com>
2024-03-21 08:16:43 +0300

    first class fumctions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(multiplier) {
+    return function(value) {
+        return value * multiplier;
+    };
+};
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+const selectDifferentDrivers = function(drivers, fn) {
+    return fn(drivers);
+};
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+console.log(returnFirstTwoDrivers(drivers)); 
+console.log(returnLastTwoDrivers(drivers)); 
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); 
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers)); 
+console.log(fareDoubler(10)); 
+console.log(fareTripler(10)); 
